704b504277f9668d1fe981e350bb94258e3c84ad
Timothy Kim <ttkim@wesleyan.edu>
2017-11-02 15:42:09 -0400

    Splitting up the two circle view controllers

--- Karma/Controllers/Circle/CircleController.swift
+++ Karma/Controllers/Circle/CircleController.swift
@@ -33,39 +33,5 @@ class CircleController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //UI Elements
-    @IBOutlet var circleNameField : UITextField!
-    
-    @IBAction func createCircle(sender : AnyObject) {
-        if (validCircle()) {
-            self.performSegue(withIdentifier: "CircleToMain", sender: self)
-        }
-    }
-    
-    //Server Call
-    func validCircle() -> Bool {
-        let backendless = Backendless.sharedInstance()!
-        let dataStore = backendless.data.of(Circle().ofClass())
-        
-        let circle = Circle(name: circleNameField.text!)
-        let currentUser = User.getCurrentUser()
-        var valid = true
 
-        Types.tryblock({ () -> Void in
-            //Save the new object, retrieve its object id, and add the relation to the Users column
-            let savedCircle = dataStore!.save(circle) as! Circle;
-            dataStore!.setRelation(
-                "Users",
-                parentObjectId: savedCircle.objectId,
-                childObjects: [currentUser.objectId]
-            )
-            currentUser.updateProperties(["circleId" : savedCircle.objectId!])
-            backendless.userService.update(currentUser)
-        }, catchblock: {(exception) -> Void in
-            print(exception ?? "Error")
-            valid = false
-        })
-    
-        return valid
-    }
 }
---
+++ Karma/Controllers/Circle/CircleCreateViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CircleCreateViewController.swift
+//  Karma
+//
+//  Created by Timothy Taeho Kim on 11/2/17.
+//  Copyright © 2017 Olya Danylova. All rights reserved.
+//
+
+import UIKit
+
+class CircleCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //UI Elements
+    @IBOutlet var circleNameField : UITextField!
+    
+    @IBAction func createCircle(sender : AnyObject) {
+        if (validCircle()) {
+            self.performSegue(withIdentifier: "CircleToMain", sender: self)
+        }
+    }
+    
+    //Server Call
+    func validCircle() -> Bool {
+        let backendless = Backendless.sharedInstance()!
+        let dataStore = backendless.data.of(Circle().ofClass())
+        
+        let circle = Circle(name: circleNameField.text!)
+        let currentUser = User.getCurrentUser()
+        var valid = true
+        
+        Types.tryblock({ () -> Void in
+            //Save the new object, retrieve its object id, and add the relation to the Users column
+            let savedCircle = dataStore!.save(circle) as! Circle;
+            dataStore!.setRelation(
+                "Users",
+                parentObjectId: savedCircle.objectId,
+                childObjects: [currentUser.objectId]
+            )
+            currentUser.updateProperties(["circleId" : savedCircle.objectId!])
+            backendless.userService.update(currentUser)
+        }, catchblock: {(exception) -> Void in
+            print(exception ?? "Error")
+            valid = false
+        })
+        
+        return valid
+    }
+}
