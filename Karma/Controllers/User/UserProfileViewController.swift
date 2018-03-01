e1900da46574b18feb012c092f1af924916e3b98
Timothy Kim <ttkim@wesleyan.edu>
2018-03-01 00:30:22 -0500

    Correctly caching user image in picking phase

--- Karma/Controllers/User/UserProfileViewController.swift
+++ Karma/Controllers/User/UserProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class UserProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -27,13 +28,14 @@ class UserProfileViewController: UIViewController, UIImagePickerControllerDelega
         imagePicker.delegate = self
         
         // imageView.image = getUserProfile()
-        _ = getUserProfile()
+        displayUserPicture()
     }
     
     @objc
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             uploadImageToServer(image: pickedImage)
+            saveImageToCache(image: pickedImage)
             imageView.image = pickedImage.maskInCircle(image: pickedImage, radius: 78)
             imageView.contentMode = .scaleAspectFit
         }
@@ -41,27 +43,35 @@ class UserProfileViewController: UIViewController, UIImagePickerControllerDelega
         dismiss(animated: true, completion: nil)
     }
     
-    private func getUserProfile() -> UIImage {
+    private func saveImageToCache (image: UIImage) {
+        let id = User.getCurrentUserId()
+        ImageCache.default.store(image, forKey: id as String)
+    }
+    
+    private func displayUserPicture() {
         let imagePath = (User.getCurrentUserProperty(key: "imagePath") as! String)
         if (imagePath == "default") {
             imageView.image = UIImage(named: "DefaultAvatar")!
+            return
         }
-        else {
-            let url = URL(string: imagePath)
-            
-            // Asynch call
-            DispatchQueue.global().async {
-                let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
-                DispatchQueue.main.async {
-                    self.imageView.image = UIImage(data: data!)
+        // Attempt to retrieve image from cache
+        ImageCache.default.retrieveImage(forKey: User.getCurrentUserId(), options: nil) {
+            image, cacheType in
+            if let image = image {
+                self.imageView.image = image
+            } else {
+                print("That image does not exist in cache, pulling from server")
+                let url = URL(string: imagePath)
+                DispatchQueue.global().async {
+                    let data = try? Data(contentsOf: url!)
+                    DispatchQueue.main.async {
+                        let onlineImage = UIImage(data: data!)
+                        self.saveImageToCache(image: onlineImage!)
+                        self.imageView.image = onlineImage
+                    }
                 }
             }
-//            let url = URL(string: imagePath)
-//            let data = try? Data(contentsOf: url!)
-//            imageView.image = UIImage(data: data!)
         }
-        
-        return UIImage(named: "DummyAvatar")!
     }
     
     private func uploadImageToServer(image: UIImage) {
