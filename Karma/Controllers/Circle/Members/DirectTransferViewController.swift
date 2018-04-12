75b1a10992c0ae7de798971a5f59b8f245b7b0e5
Timothy Kim <ttkim@wesleyan.edu>
2018-04-12 01:26:12 -0400

    Dropdown working on a basic level getting names

--- Karma/Controllers/Circle/Members/DirectTransferViewController.swift
+++ Karma/Controllers/Circle/Members/DirectTransferViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import DropDown
 
 class DirectTransferViewController: UIViewController, KeyboardDelegate, UITextViewDelegate {
 
     // Local Variables
+    let dropDown = DropDown()
     var currentTransfer : DirectTransfer!
+    var members : [NSDictionary]!
+    //var members : NSDictionary!
     var origButtonPosition : CGFloat!
     var numPad = NumPadCalculator(frame: CGRect(x: 0, y: 0, width: 375, height: 216))
     var placeholderLabel : UILabel!
+    var userName : String!
 
     // UI Elements
+    @IBOutlet weak var dropDownView : UIView!
     @IBOutlet weak var costField : UITextField!
-    @IBOutlet weak var selectedUser : UILabel!
+    @IBOutlet weak var selectedUser : UIButton!
     @IBOutlet weak var descriptionField : UITextView!
     @IBOutlet weak var requestButton : UIButton!
     @IBOutlet weak var payButton : UIButton!
@@ -34,6 +40,107 @@ class DirectTransferViewController: UIViewController, KeyboardDelegate, UITextVi
         setupDetails()
     }
 
+    // MARK: Setup Methods
+    func setupView() {
+        costField.becomeFirstResponder()
+        
+        loadDropDown()
+        
+        loadVariables()
+        
+        loadMembers()
+    }
+    
+    private func loadDropDown() {
+        dropDown.anchorView = dropDownView
+        dropDown.bottomOffset = CGPoint(x: 0, y:(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            self.selectedUser.setTitle(item, for: .normal)
+        }
+    }
+    
+    private func loadVariables() {
+        self.members = []
+        self.origButtonPosition = requestButton.frame.origin.y
+        if let currentUserId = UserUtil.getCurrentId() {
+            UserUtil.getCurrentUserName() { currentUserName in
+                UserUtil.getCurrentProperty(key: "name") { currentName in
+                    self.userName = currentUserName
+                    self.currentTransfer = DirectTransfer(
+                        currentUserId: currentUserId,
+                        currentUserName : currentUserName,
+                        currentName: currentName as? String ?? ""
+                    )
+                }
+            }
+        }
+    }
+    
+    private func loadMembers() {
+        // Convert the members to a list
+        Circle.getProperty(key: "members") { members in
+            if let membersDic = members as? NSDictionary {
+                for (userName, val) in membersDic {
+                    if let userName = userName as? String {
+                        // Remove the current user from the list
+                        if (self.userName != userName) {
+                            let memberDic : NSDictionary = [userName : val]
+                            self.members.append(memberDic)
+                        }
+                    }
+                }
+                // Sort the list by the userName
+                self.members.sort { (firstDic, secondDic) -> Bool in
+                    let firstUser = Array(firstDic.allKeys).first as! String
+                    let secondUser = Array(secondDic.allKeys).first as! String
+                    return firstUser == secondUser ? true : firstUser < secondUser
+                }
+            }
+        }
+    }
+    
+    func setupKeyboard() {
+        // These are added to push the Request button to appear above the keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        // Adding costField to use the numPad view
+        numPad.delegate = self
+        costField.inputView = numPad
+        
+        // Dismissing numPad should evaluate the current expression
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DirectTransferViewController.finishCompute))
+        view.addGestureRecognizer(tap)
+        
+        // Turn off autocorrect/auto predict for the description field
+        descriptionField.autocorrectionType = .no
+    }
+    
+    func setupDetails() {
+        // Add ghost text to the details field
+        descriptionField!.delegate = self
+        placeholderLabel = UILabel()
+        placeholderLabel.text = "What is this for?"
+        placeholderLabel.sizeToFit()
+        descriptionField.addSubview(placeholderLabel)
+        placeholderLabel.frame.origin = CGPoint(x: 5, y: (descriptionField.font?.pointSize)! / 2)
+        placeholderLabel.textColor = UIColor.lightGray
+        placeholderLabel.isHidden = !descriptionField.text.isEmpty
+    }
+    
+    @IBAction func displayMembers(sender : AnyObject) {
+        var names : [String] = []
+        for dict in self.members {
+            for (_, val) in dict {
+                if let info = val as? NSDictionary, let name = info["name"] as? String {
+                    names.append(name)
+                }
+            }
+        }
+        dropDown.dataSource = names
+        dropDown.show()
+    }
+    
     // Pay/Request Handling
     @IBAction func request(sender : AnyObject) {
         if validValues() {
@@ -77,49 +184,12 @@ class DirectTransferViewController: UIViewController, KeyboardDelegate, UITextVi
 
         return true
     }
-
-    // MARK: Setup Methods
-    func setupView() {
-        // Set the text of the user label
-        selectedUser.text = currentTransfer.selectedName
-        self.origButtonPosition = requestButton.frame.origin.y
-        costField.becomeFirstResponder()
-    }
     
-    func setupKeyboard() {
-        // These are added to push the Request button to appear above the keyboard
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-        
-        // Adding costField to use the numPad view
-        numPad.delegate = self
-        costField.inputView = numPad
-        
-        // Dismissing numPad should evaluate the current expression
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DirectTransferViewController.finishCompute))
-        view.addGestureRecognizer(tap)
-        
-        // Turn off autocorrect/auto predict for the description field
-        descriptionField.autocorrectionType = .no
-    }
-    
-    func setupDetails() {
-        // Add ghost text to the details field
-        descriptionField!.delegate = self
-        placeholderLabel = UILabel()
-        placeholderLabel.text = "What is this for?"
-        placeholderLabel.sizeToFit()
-        descriptionField.addSubview(placeholderLabel)
-        placeholderLabel.frame.origin = CGPoint(x: 5, y: (descriptionField.font?.pointSize)! / 2)
-        placeholderLabel.textColor = UIColor.lightGray
-        placeholderLabel.isHidden = !descriptionField.text.isEmpty
-    }
-    
+    // MARK: Helper Functions
     func textViewDidChange(_ textView: UITextView) {
         placeholderLabel.isHidden = !descriptionField.text.isEmpty
     }
     
-    // MARK: Helper Functions
     @objc func finishCompute() {
         NumPadCalculator.computeOperation(numPad)()
         view.endEditing(true)
